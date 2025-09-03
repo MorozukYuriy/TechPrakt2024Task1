b86d04675d3db838b217929feaecf5863bb58a0f
Morozuk Yuriy <ipz222_myua@student.ztu.edu.ua>
2025-09-04 00:06:18 +0300

    виправлено конфлікти деструктуризації, десяткове число 10.5, додано приклад spread-виклику

--- jsdemo/js/index.js
+++ jsdemo/js/index.js
@@ -1,3 +1,5 @@
+// jsdemo/js/index.js
+"use strict";
 
 class User {
     #login;
@@ -9,13 +11,13 @@ class User {
     }
 
     getLogin() {
-        console.log(this);
         return this.#login;
     }
 
     testPassword(password) {
         return this.#password === password;
     }
+
     display() {
         console.log(this.#login);
     }
@@ -29,35 +31,38 @@ class Admin extends User {
         this.#role = role;
     }
 }
-const user = new User('admin', '123456');
 
-let panelParams = {
-    //width : 300,
-    title : 'Title',
-    height : 200,
-    age : 30
+const user = new User("admin", "123456");
+
+const panelParams = {
+    // width: 300,
+    title: "Title",
+    height: 200,
+    age: 30,
 };
 
-let {title, height} = panelParams;
-let [id, title, count, price] = [1, 'title', 10, 10,5];
+// уникаємо конфлікту назв: беремо title як panelTitle
+const { title: panelTitle, height } = panelParams;
+
+// коректна кількість елементів та десяткове число 10.5
+const [id, rawTitle, count, price] = [1, "title", 10, 10.5];
+
+// своп значень
 let x = 1;
 let y = 2;
 [x, y] = [y, x];
-
-
 
 function func2(a, b, c) {
     console.log(a);
     console.log(b);
     console.log(c);
 }
-/*let arr2 = [1,2,3,4,5];
-func2(...arr2);*/
 
-/*
-let myf = user.getLogin;
-console.log(myf.bind(user));
+// приклад коректного виклику через spread
+const arr2 = [1, 2, 3];
+func2(...arr2);
 
-console.log(user.getLogin());
-const admin = new Admin('admin', '123456', 'all');
-*/
+// приклад використання методів
+// console.log(user.getLogin());
+// const admin = new Admin("admin", "123456", "all");
+// console.log(admin.testPassword("123456"));
